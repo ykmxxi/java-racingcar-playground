43c2799ddd400a94d1d460b713effd69c72792f3
ykmxxi <ykmxxi97@gmail.com>
2023-10-09 15:14:13 +0900

    feat(Validator): 기본 구분자인지 검사하는 기능 구현

--- src/main/java/domain/Validator.java
+++ src/main/java/domain/Validator.java
@@ -3,6 +3,7 @@ package domain;
 public class Validator {
 
 	private static final String NUMBER_REGEX = "[1-9]\\d*";
+	private static final String BASIC_DELIMITER_REGEX = "[,:]";
 
 	public boolean isNumber(String input) {
 		return input.matches(NUMBER_REGEX);
@@ -10,6 +11,10 @@ public class Validator {
 
 	public boolean isNullOrEmpty(String input) {
 		return input == null || input.isEmpty();
+	}
+
+	public boolean isBasicDelimiter(String input) {
+		return input.matches(BASIC_DELIMITER_REGEX);
 	}
 
 }
