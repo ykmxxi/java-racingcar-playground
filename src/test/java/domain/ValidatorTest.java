37ccbcead77590cdf4a959b7a8f4aad38f567da3
ykmxxi <ykmxxi97@gmail.com>
2023-10-09 14:52:44 +0900

    test(Validator): 숫자인지 검사하는 기능 테스트

---
+++ src/test/java/domain/ValidatorTest.java
@@ -0,0 +1,35 @@
+package domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ValidatorTest {
+
+	private final Validator validator = new Validator();
+
+	@Test
+	void 숫자_검증_통과() {
+		// given
+		String input = "1";
+
+		// when
+		boolean result = validator.isNumber(input);
+
+		// then
+		assertThat(result).isTrue();
+	}
+
+	@Test
+	void 숫자_검증_실패() {
+		// given
+		String input = "one";
+
+		// when
+		boolean result = validator.isNumber(input);
+
+		// then
+		assertThat(result).isFalse();ø
+	}
+
+}
