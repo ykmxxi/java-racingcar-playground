83ec7e24949f4120dabe7a4c410f2c9536b79d3a
ykmxxi <ykmxxi97@gmail.com>
2023-10-09 14:43:28 +0900

    feat(View):입력의 결과를 출력하는 기능

--- src/main/java/domain/View.java
+++ src/main/java/domain/View.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 public class View {
 
 	private static final Scanner scanner = new Scanner(System.in);
+	private static final String RESULT_ARROW = " => ";
 
 	public String readString() {
 		return scanner.nextLine();
+	}
+
+	public void printResult(String input, String result) {
+		System.out.println(input + RESULT_ARROW + result);
 	}
 
 }
