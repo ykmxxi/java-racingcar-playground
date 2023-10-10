89688f69fdb8c6ae2298079e89ea4bef088e0c60
ykmxxi <ykmxxi97@gmail.com>
2023-10-10 14:53:35 +0900

    feat(InputView): 자동차 이름들을 입력받는 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,19 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+	private static final Scanner scanner = new Scanner(System.in);
+	private static final String ERROR_MESSAGE = "값을 입력해 주세요.";
+
+	public String readCarNames() {
+		String carNames = scanner.nextLine();
+		if (isEmptyOrWhitespaces(carNames)) {
+			throw new RuntimeException(ERROR_MESSAGE);
+		}
+		return carNames;
+	}
 
 	public boolean isEmptyOrWhitespaces(String input) {
 		return input.isBlank();
