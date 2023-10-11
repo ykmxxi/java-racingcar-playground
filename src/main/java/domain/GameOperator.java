b8c7208fe7a39c46e694c4a14043be14d20a219d
ykmxxi <ykmxxi97@gmail.com>
2023-10-11 17:40:25 +0900

    feat(GameOperator): 입력받은 자동차 이름을 구분해 알려주는 기능 구현

--- src/main/java/domain/GameOperator.java
+++ src/main/java/domain/GameOperator.java
@@ -1,11 +1,20 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameOperator {
 
 	private static final int FOUR = 4;
 
 	public boolean isGreaterThanOrEqualThanFour() {
 		return RandomNumberGenerator.createRandomNumber() >= FOUR;
+	}
+
+	public List<String> splitCarNames(String carNames) {
+		return Arrays.stream(carNames.split(","))
+					 .collect(Collectors.toUnmodifiableList());
 	}
 
 }
