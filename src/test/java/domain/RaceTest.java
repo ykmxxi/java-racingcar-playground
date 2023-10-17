798066e01aeca9f715430b82cea2535cc254218a
ykmxxi <ykmxxi97@gmail.com>
2023-10-17 15:54:41 +0900

    test(Race): 우승자를 알려주는 기능 테스트

--- src/test/java/domain/RaceTest.java
+++ src/test/java/domain/RaceTest.java
@@ -27,4 +27,24 @@ class RaceTest {
 		assertThat(results).isEqualTo(expected);
 	}
 
+	@Test
+	void 우승자를_알려주는_기능() {
+		// given
+		Race race1 = new Race(List.of(new Car("pobi"), new Car("crong")), 0);
+		Race race2 = new Race(List.of(new Car("pobi"), new Car("crong")), 0);
+
+		// when
+		List<Car> cars1 = race1.getCars();
+		cars1.get(0).updatePosition();
+		cars1.get(0).updatePosition();
+		cars1.get(1).updatePosition();
+
+		race2.getCars()
+			 .forEach(Car::updatePosition);
+
+		// then
+		assertThat(race1.notifyChampionNames()).isEqualTo(List.of("pobi"));
+		assertThat(race2.notifyChampionNames()).isEqualTo(List.of("pobi", "crong"));
+	}
+
 }
